156afe801d133677f69b1780da627af4968aaceb
Kieran Wood <kieranw098@gmail.com>
2020-01-14 22:17:25 -0700

    Added Tarball and custom PPA support

--- pystall/core.py
+++ pystall/core.py
@@ -58,6 +58,7 @@ import logging                      # Used to grab logs from functions and class
 
 import subprocess                   # Used to install/run binaries once downloaded
 from zipfile import ZipFile         # Used to extract files from .zip archives
+import tarfile
 from abc import ABC, abstractmethod # Used to enforce subclassing from base Resource class
 
 # Third party dependencies
@@ -439,7 +440,7 @@ class DEBResource(Resource):
     Examples
     --------
     ```
-    from pystall.core import MSIResource, build
+    from pystall.core import DEBResource, build
 
     atom = DEBResource("Atom", "https://atom.io/download/deb")
 
@@ -465,6 +466,141 @@ class DEBResource(Resource):
             logging.info(f"Removing installer {self.label}")
             os.remove(self.location)
 
+
+class CUSTOMPPAResource:
+    """
+
+    Attributes
+    ----------
+
+    label : (str)
+        Human readable name for resource
+    
+    PPA : (str)
+        The name of the ppa to add
+    
+    packages : (list|str)
+        Specify either a list of packages to install, or a string with a package name.
+
+    Methods
+    -------
+    download:
+        Adds specified PPA and apt-get updates
+
+    install:
+        Installs specified packages after the PPA had been added.
+
+    Examples
+    --------
+    ```
+    from pystall.core import CUSTOMPPAResource, build
+
+    python_linux = CUSTOMPPAResource("Python 3.8", "deadsnakes/ppa", ["python3.7", "python3.8"])
+
+    build(python_linux)
+
+    ```
+
+    """
+    def __init__(self, label, PPA, packages):
+        self.label = label
+        self.PPA = PPA
+        self.packages = packages
+        self.downloaded = False
+        
+    def download(self):
+        """Adds PPA and apt updates"""
+
+        logging.info(f"Adding PPA {self.PPA}")
+        installer = subprocess.Popen(f"sudo add-apt-repository ppa:{self.PPA} && sudo apt-get update", shell=True)
+
+        while installer.poll() == None:
+            """loop runs until process has terminated"""
+
+    def install(self):
+        """Installs specified packages"""
+        logging.info(f"Installing {self.label}")
+
+        if type(self.packages) == str:
+            logging.info(f"Installing{self.packages}")
+            installer = subprocess.Popen(f"sudo apt install {self.packages}", shell=True)
+
+            while installer.poll() == None:
+                """loop runs until process has terminated"""
+
+        elif (type(self.packages) == list) or (type(self.packages) == tuple):
+            for package in self.packages:
+                logging.info(f"Installing {package}")
+                installer = subprocess.Popen(f"sudo apt install {package}", shell=True)
+
+                while installer.poll() == None:
+                    """loop runs until process has terminated"""
+
+class TARBALLResource(Resource):
+    """Used to download and extract .tar.gz files.
+
+    Attributes
+    ----------
+
+    label : (str)
+        Human readable name for resource and used with extension in files name.
+    
+    location : (str)
+        The path or URL to the resource that needs to be downloaded & installed
+    
+    arguments : (list|bool)
+        Specify any arguments to be passed on installation, False indicates no arguments.
+    
+    downloaded : (bool)
+        Used to delineate if Resource is downloaded, if using local file set to True, else leave as False.
+
+    remove: (bool)
+        Whether to delete the .zip after installation, by default True.
+
+    Methods
+    -------
+    download:
+        Downloads Resource from location specified in self.location of the instance
+
+    install:
+        Extracts the .zip file.
+        NOTE: assumes you have already downloaded the file or set the self.location to correct file path.
+
+    Examples
+    --------
+    ```
+    from pystall.core import ZIPResource, build
+
+    micro = ZIPResource("micro editor", "https://github.com/zyedidia/micro/releases/download/v1.4.1/micro-1.4.1-win64.zip")
+
+    build(micro)
+    ```
+    """
+    def __init__(self, label, location, arguments = False, downloaded = False, remove = True):
+        super().__init__(label, ".tar.gz", location, arguments, downloaded)
+        self.remove = remove
+
+    def extract(self):
+        """Extracts the .tar.gz file."""
+        extract_path = self.location[:-7:]
+        logging.info(f"Extracting Zip archive {self.location} to {extract_path}")
+        with tarfile.open(name = self.location, mode = "r") as archive:
+                archive.extractall(extract_path) # Strip extension and extract to folder
+
+    def install(self):
+        """Extracts the .zip file and runs necessary installation steps. NOTE: Not yet implemented"""
+        if self.downloaded:
+            logging.info(f"Installing {self.label}")
+            self.extract()
+            
+        else:
+            logging.error(f"{self.name} failed to install due to not being downloaded")
+
+        if self.remove:
+            logging.info(f"Removing installer {self.label}")
+            os.remove(self.location)
+
+
 def build(*resources):
     """downloads and installs everything specified"""
     for resource in resources:
@@ -486,7 +622,11 @@ if __name__ == "__main__": # Used to test out functionality while developing
    
     atom = DEBResource("Atom", "https://atom.io/download/deb", remove=False)
 
-    build(atom)
+    python_linux = CUSTOMPPAResource("Python 3.8", "deadsnakes/ppa", ["python3.7", "python3.8"])
+
+    micro_linux = TARBALLResource("Micro editor", "https://github.com/zyedidia/micro/releases/download/v1.4.1/micro-1.4.1-linux64.tar.gz", remove=False)
+
+    build(micro_linux)
 
     # Need to test this more
     # path = f"C:\\Users\\Kieran\\Downloads\\micro editor\\micro-1.4.1"
